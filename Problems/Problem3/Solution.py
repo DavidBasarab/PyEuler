ba6bc4ea5d29886e1a7840d6b9154a91263723f4
David Basarab <dbasarab617@gmail.com>
2020-11-14 10:39:22 -0500

    solved problem 3

--- Problems/Problem3/Solution.py
+++ Problems/Problem3/Solution.py
@@ -4,16 +4,37 @@ def execute():
 	is_prime = lambda number: all(number % i != 0 for i in range(2, int(number ** .5) + 1))
 
 	def prime_series(max_number):
-		print(f'Going to list all primes to {max_number}')
+		primes = []
 		for index in range(2, max_number):
 			if is_prime(index):
-				print(index, end=" ")
+				primes.append(index)
 			else:
 				pass
 
-	range_number = int(input("Enter the input Range :"))
+		return primes
 
-	prime_series(range_number)
+	prime_numbers = prime_series(13195)
+
+	def find_prime_factors(number):
+		if is_prime(number):
+			return [number]
+
+		factors = []
+
+		for current_prime in prime_numbers:
+			if number % current_prime == 0:
+				factors.append(current_prime)
+				next_number = int(number / current_prime)
+				factors = factors + find_prime_factors(next_number)
+				break
+
+		return factors
+
+	prime_factors = find_prime_factors(600851475143)
+
+	print(prime_factors)
+
+	print(f'Largest Prime Number of 600851475143 is {prime_factors[-1]}')
 
 	print('')
 
