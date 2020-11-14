5f769f527cce09260393da1d6155a2a1c09fb3da
David Basarab <dbasarab617@gmail.com>
2020-11-13 23:07:46 -0500

    better organization

--- Runner.py
+++ Runner.py
@@ -3,7 +3,8 @@ from importlib import import_module
 
 print('')
 
-mod = import_module(f'Problem{sys.argv[1]}')
+mod = import_module(f'Problems.Problem{sys.argv[1]}.Problem{sys.argv[1]}')
+
 met = getattr(mod, 'execute')
 
 met()
